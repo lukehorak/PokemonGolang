5d169daf538954ccc6847fda77cc150a7278d44b
lukehorak <lukasbhorak@gmail.com>
2020-08-14 12:05:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# OUBL Summary
+
+
+This repo contain Go functions used to get summaries of Pokemon Showdown battles (uploaded as Google Cloud functions)
+
+# Coming Soon
+
+- Scoring system documentation
+- The actual functions
---
+++ WillLukeWin.go
@@ -0,0 +1,26 @@
+// Package p contains an HTTP Cloud Function.
+package p
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+)
+
+// Will I win the OUBL this season?
+func WillLukeWin(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Message string `json:"message"`
+		Number  int    `json:"number"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Of course he wont, he's terrible at Pokemon!")
+		return
+	}
+	if d.Message == "" {
+		fmt.Fprint(w, "Of course he wont, he's terrible at Pokemon!")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
+}
